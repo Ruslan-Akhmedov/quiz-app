684a5feef9a829e8aa4c177d43003a388e64c748
Ruslan Akhmedov <rus.axmedov.1998@inbox.ru>
2026-02-25 21:18:40 +0300

    Добавил страницу с вопросами и вопросы

--- src/QuizQuestion/index.css.ts
+++ src/QuizQuestion/index.css.ts
@@ -20,4 +20,57 @@ export const container = style({
   background: "white",
 });
 
-export const question = style({});
+export const line = style({
+  width: "100%",
+  height: "2px",
+  backgroundColor: "black",
+});
+
+export const question = style({
+  display: "flex",
+  minWidth: "500px",
+  border: "1px solid black",
+  borderRadius: "8px",
+  padding: "10px",
+});
+
+export const variants = style({
+  display: "grid",
+  gridTemplateColumns: "repeat(2, 1fr)",
+  gap: "12px",
+});
+
+export const variant = style({
+  display: "flex",
+  minWidth: "235px",
+  padding: "10px",
+  border: "1px solid black",
+  borderRadius: "8px",
+  justifyContent: "center",
+  textAlign: "center",
+});
+
+export const buttonContainer = style({
+  display: "flex",
+  gap: "12px",
+});
+
+export const nextQuestion = style({
+  display: "flex",
+  minWidth: "255px",
+  padding: "10px",
+  border: "1px solid black",
+  borderRadius: "8px",
+  justifyContent: "center",
+  textAlign: "center",
+});
+
+export const quizEnd = style({
+  display: "flex",
+  minWidth: "255px",
+  padding: "10px",
+  border: "1px solid black",
+  borderRadius: "8px",
+  justifyContent: "center",
+  textAlign: "center",
+});
--- src/QuizQuestion/index.tsx
+++ src/QuizQuestion/index.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import * as styles from "./index.css";
+import { Questions } from "../constants/index";
 
 export const QuizQuestion = () => {
-  return (
+  const [nextQuestion, setNextQuestion] = useState(1);
+
+  // const handleNextQuestion = () = {
+  //   setNextQuestion(nextQuestion)
+  // }
+
+  return Questions.map((question, index) => (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <div className={styles.question}></div>
-        <div></div>
+        Вопрос {question.id}/{Questions.length}
+        <hr className={styles.line} />
+        <div className={styles.question}>{question.text}</div>
+        <div className={styles.variants}>
+          {question.variants.map((variant, index) => (
+            <div className={styles.variant} key={index}>
+              {variant}
+            </div>
+          ))}
+        </div>
+        <div className={styles.buttonContainer}>
+          <button className={styles.quizEnd}>Завершить квиз</button>
+          <button
+            className={styles.nextQuestion}
+            onClick={() => setNextQuestion(nextQuestion + 1)}
+          >
+            Следующий вопрос
+          </button>
+        </div>
       </div>
     </div>
-  );
+  ));
 };
